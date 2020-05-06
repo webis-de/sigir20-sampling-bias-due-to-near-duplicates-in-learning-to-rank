11c5af9cd3422e97fdf147611bb30d3deed15e16
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2020-05-06 02:50:57 +0200

    Read local data path from properties

--- source/data/build.gradle.kts
+++ source/data/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     kotlin("jvm")
 }
@@ -13,5 +15,29 @@ tasks {
         description = "Print current Webis Ceph, and thesis data directories."
         main = "de.webis.webisstud.thesis.reimer.data.Data"
         classpath = sourceSets.main.get().runtimeClasspath
+    }
+
+    val createProperties = register("createLocalDataProperties") {
+        val dataPath = rootProject.file("data").absolutePath
+        val propertiesFile = sourceSets
+            .main
+            .get()
+            .resources
+            .sourceDirectories
+            .first()
+            .resolve("data.properties")
+        doLast {
+            val properties = Properties()
+            properties["data.path"] = dataPath
+            propertiesFile.writer().use {
+                properties.store(it, null)
+            }
+        }
+        inputs.property("data path", dataPath)
+        outputs.file(propertiesFile)
+    }
+
+    classes {
+        dependsOn(createProperties)
     }
 }
--- source/data/src/main/kotlin/de/webis/webisstud/thesis/reimer/data/Data.kt
+++ source/data/src/main/kotlin/de/webis/webisstud/thesis/reimer/data/Data.kt
@@ -1,9 +1,12 @@
 package de.webis.webisstud.thesis.reimer.data
 
+import de.webis.webisstud.thesis.reimer.model.Resource
+import de.webis.webisstud.thesis.reimer.model.resource
 import java.io.File
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 object Data {
     @JvmStatic
@@ -12,24 +15,30 @@ object Data {
         println("Current data directory: $dataDir")
     }
 
+    private val dataProperties = Properties().apply {
+        load(Resource("/data.properties", Data::class).inputStream())
+    }
+
     private class PathsDirectory(private vararg val paths: String?) {
-        val directoryOrNull
+        val pathFiles
             get() = paths
-                    .asSequence()
-                    .filterNotNull()
-                    .map(::File)
-                    .filter(File::exists)
-                    .filter(File::isDirectory)
-                    .firstOrNull()
+                .asSequence()
+                .filterNotNull()
+                .map(::File)
+        val directoryOrNull
+            get() = pathFiles
+                .filter(File::exists)
+                .filter(File::isDirectory)
+                .firstOrNull()
         val directory
             get() = directoryOrNull
-                    ?: error("Could not find directory, searched in: ${paths.filterNotNull().joinToString()}")
+                ?: error("Could not find directory, searched in: ${pathFiles.joinToString { it.absolutePath }}")
     }
 
     private val webisCephPaths = PathsDirectory(
-            System.getenv("WEBIS_CEPH_HOME"),
-            "/mnt/ceph/storage/",
-            "W:/"
+        System.getenv("WEBIS_CEPH_HOME"),
+        "/mnt/ceph/storage/",
+        "W:/"
     )
 
     private val webisCephDir by lazy { webisCephPaths.directory }
@@ -39,33 +48,30 @@ object Data {
     }
 
     private val dataPaths = PathsDirectory(
-            System.getenv("BACHELOR_THESIS_DATA"),
-            webisCephPaths.directoryOrNull?.resolve("data-in-progress/wstud-thesis-reimer")?.path,
-            "data"
+        dataProperties.getProperty("data.path"),
+        System.getenv("BACHELOR_THESIS_DATA"),
+        webisCephPaths.directoryOrNull?.resolve("data-in-progress/wstud-thesis-reimer")?.path,
+        "data"
     )
 
-    private val dataDir = dataPaths.directory
+    private val dataDir by lazy { dataPaths.directory }
 
-    internal val corporaDir = dataDir.resolve("corpora")
+    internal val corporaDir by lazy { dataDir.resolve("corpora").apply { mkdir() } }
 
-    internal val tasksDir = dataDir.resolve("tasks")
+    internal val tasksDir by lazy { dataDir.resolve("tasks").apply { mkdir() } }
 
-    private val datasetsDir = dataDir.resolve("datasets")
+    val featuresDir by lazy { dataDir.resolve("features").apply { mkdir() } }
 
-    val letorDir = datasetsDir.resolve("letor")
-
-    val experimentsDir = dataDir.resolve("experiments")
+    private val experimentsDir by lazy { dataDir.resolve("experiments").apply { mkdir() } }
 
     private val formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd-HH-mm")
 
     fun experimentsDir(time: Instant) = experimentsDir
-            .resolve(formatter.format(time.atOffset(ZoneOffset.UTC)))
+        .resolve(formatter.format(time.atOffset(ZoneOffset.UTC)))
 
     fun newExperimentsDir() = experimentsDir(Instant.now())
 
-    val tempDir = dataDir.resolve("tmp")
+    private val tempDir by lazy { dataDir.resolve("tmp").apply { mkdir() } }
 
     fun newTempDir() = createTempDir(directory = tempDir)
-
-    fun newTempFile() = createTempFile(directory = tempDir)
 }
